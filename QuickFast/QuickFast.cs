1835d9eca8c42b001f72e0b19a15a81cccf47cbe
Dubwise <dubwise56@gmail.com>
2020-02-11 13:37:24 +0000

    Initial commit

---
+++ QuickFast/QuickFast.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Harmony;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace QuickFast
+{
+
+
+    public class QuickFast : Mod
+    {
+        public QuickFast(ModContentPack content) : base(content)
+        {
+            HarmonyInstance.Create("QuickFast").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(JobDriver_Wear))]
+    [HarmonyPatch(nameof(JobDriver_Wear.Notify_Starting))]
+    public static class gert
+    {
+        public static void Postfix(JobDriver_Wear __instance)
+        {
+            __instance.duration = 10;
+        }
+    }
+
+
+    [HarmonyPatch(typeof(JobDriver_RemoveApparel))]
+    [HarmonyPatch(nameof(JobDriver_RemoveApparel.Notify_Starting))]
+    public static class shart
+    {
+        public static void Postfix(JobDriver_Wear __instance)
+        {
+            __instance.duration = 10;
+        }
+    }
+}
